2dd6e4d5480548c775b6cd27b24e8eb146997616
amosschann <71762034+amosschann@users.noreply.github.com>
2025-04-03 13:47:00 +0800

    Update MediaPicker.m
    use non compression method for video

--- src/ios/MediaPicker.m
+++ src/ios/MediaPicker.m
@@ -67,7 +67,7 @@
                 if(asset.mediaType==PHAssetMediaTypeImage){
                     [self imageToSandbox:asset dmcPickerPath:dmcPickerPath aListArray:aListArray selectArray:selectArray index:index];
                 }else{
-                    [self videoToSandboxCompress:asset dmcPickerPath:dmcPickerPath aListArray:aListArray selectArray:selectArray index:index];
+                    [self videoToSandbox:asset dmcPickerPath:dmcPickerPath aListArray:aListArray selectArray:selectArray index:index];
                 }
             }
             index++;
@@ -141,41 +141,57 @@
 }
 
 
--(void)videoToSandbox:(PHAsset *)asset dmcPickerPath:(NSString*)dmcPickerPath aListArray:(NSMutableArray*)aListArray selectArray:(NSMutableArray*)selectArray index:(int)index{
-    PHImageRequestOptions *options = [[PHImageRequestOptions alloc] init];
+-(void)videoToSandbox:(PHAsset *)asset dmcPickerPath:(NSString*)dmcPickerPath aListArray:(NSMutableArray*)aListArray selectArray:(NSMutableArray*)selectArray index:(int)index {
+    PHVideoRequestOptions *options = [[PHVideoRequestOptions alloc] init];
     options.networkAccessAllowed = YES;
-    options.resizeMode = PHImageRequestOptionsResizeModeFast;
     options.progressHandler = ^(double progress, NSError *error, BOOL *stop, NSDictionary *info) {
-        NSString *compressCompletedjs = [NSString stringWithFormat:@"MediaPicker.icloudDownloadEvent(%f,%i)", progress,index];
-        [self.commandDelegate evalJs:compressCompletedjs];
+        if (error) {
+            NSLog(@"Error downloading video: %@", error.localizedDescription);
+        }
+        NSString *progressJS = [NSString stringWithFormat:@"MediaPicker.icloudDownloadEvent(%f,%i)", progress, index];
+        [self.commandDelegate evalJs:progressJS];
     };
+
     [[PHImageManager defaultManager] requestAVAssetForVideo:asset options:options resultHandler:^(AVAsset *avsset, AVAudioMix *audioMix, NSDictionary *info) {
         if ([avsset isKindOfClass:[AVURLAsset class]]) {
+            AVURLAsset *urlAsset = (AVURLAsset *)avsset;
             NSString *filename = [asset valueForKey:@"filename"];
-            AVURLAsset* urlAsset = (AVURLAsset*)avsset;
-            
-            NSString *fullpath=[NSString stringWithFormat:@"%@/%@", dmcPickerPath,filename];
-            NSLog(@"%@", urlAsset.URL);
-            NSData *data = [NSData dataWithContentsOfURL:urlAsset.URL options:NSDataReadingUncached error:nil];
+            NSString *fullpath = [NSString stringWithFormat:@"%@/%@", dmcPickerPath, filename];
 
-            NSNumber* size=[NSNumber numberWithLong: data.length];
+            NSLog(@"Video URL: %@", urlAsset.URL);
+
             NSError *error = nil;
-            if (![data writeToFile:fullpath options:NSAtomicWrite error:&error]) {
-                NSLog(@"%@", [error localizedDescription]);
-                [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:[error localizedDescription]] callbackId:callbackId];
-            } else {
-                
-                NSDictionary *dict=[NSDictionary dictionaryWithObjectsAndKeys:fullpath,@"path",[[NSURL fileURLWithPath:fullpath] absoluteString],@"uri",size,@"size",@"video",@"mediaType" ,[NSNumber numberWithInt:index],@"index", nil];
-                [aListArray addObject:dict];
-                if([aListArray count]==[selectArray count]){
-                    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:aListArray] callbackId:callbackId];
-                }
+            NSData *data = [NSData dataWithContentsOfURL:urlAsset.URL options:NSDataReadingUncached error:&error];
+
+            if (error) {
+                NSLog(@"Error reading video data: %@", error.localizedDescription);
+                [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription] callbackId:callbackId];
+                return;
             }
-           
+
+            NSNumber *size = [NSNumber numberWithLong:data.length];
+
+            if (![data writeToFile:fullpath options:NSAtomicWrite error:&error]) {
+                NSLog(@"Error writing file: %@", error.localizedDescription);
+                [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:error.localizedDescription] callbackId:callbackId];
+                return;
+            }
+
+            NSDictionary *dict = @{@"path": fullpath,
+                                   @"uri": [[NSURL fileURLWithPath:fullpath] absoluteString],
+                                   @"size": size,
+                                   @"mediaType": @"video",
+                                   @"index": @(index)};
+
+            [aListArray addObject:dict];
+
+            if ([aListArray count] == [selectArray count]) {
+                [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:aListArray] callbackId:callbackId];
+            }
         }
     }];
-
 }
+
 
 -(void)videoToSandboxCompress:(PHAsset *)asset dmcPickerPath:(NSString*)dmcPickerPath aListArray:(NSMutableArray*)aListArray selectArray:(NSMutableArray*)selectArray index:(int)index{
     NSString *compressStartjs = [NSString stringWithFormat:@"MediaPicker.compressEvent('%@',%i)", @"start",index];
